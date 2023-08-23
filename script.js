9390055bf545b71c439f293e40f32373f6d605d0
Pratik Tandel <89080177+pratik-tandel@users.noreply.github.com>
2023-08-23 15:20:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let previousWindow;
+
+document.getElementById('openNewWindowButton').addEventListener('click', function() {
+  // Close the previous window if it exists
+  if (previousWindow) {
+    previousWindow.close();
+  }
+
+  // Open a new window
+  const newWindow = window.open('https://www.example.com', '_blank', 'width=800,height=600');
+  
+  // Store the reference to the new window as the previous window
+  previousWindow = newWindow;
+});
